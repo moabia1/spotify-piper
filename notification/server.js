b5e948ab6cf96e31da90b4972ec86fec55e8b74f
moabia1 <mdmoabia81@gmail.com>
2025-10-18 14:11:59 +0530

    set-up nodemailer functionality

---
+++ notification/server.js
@@ -0,0 +1,7 @@
+import app from "./src/app";
+
+
+
+app.listen(3001, () => {
+  console.log("Notification server running on port 3001")
+})
---
+++ notification/src/config/config.js
@@ -0,0 +1,15 @@
+import { config as dotenvConfig } from "dotenv";
+
+dotenvConfig();
+
+const _config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  REFRESH_TOKEN: process.env.REFRESH_TOKEN,
+  EMAIL_USER: process.env.EMAIL_USER,
+  JWT_SECRET: process.env.JWT_SECRET,
+  MONGO_URI: process.env.MONGO_URI,
+};
+
+
+export default Object.freeze(_config)
