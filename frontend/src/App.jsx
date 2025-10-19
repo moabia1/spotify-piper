95791b4eb04d737a0393b69f3de266bac0bb880c
moabia1 <mdmoabia81@gmail.com>
2025-10-20 01:37:06 +0530

    start implementing frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Mainroutes from "./routes/Mainroutes";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app-root">
+      <Mainroutes />
+    </div>
+  );
+};
+
+export default App;
