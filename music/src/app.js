14b44dfd96e67cb0774d8bcc746fa355de5f7209
moabia1 <mdmoabia81@gmail.com>
2025-10-20 23:25:13 +0530

    set-up successfully basic server now start working on S3 bucket

--- music/src/app.js
+++ music/src/app.js
@@ -1,7 +1,12 @@
 import express from "express"
+import musicRoutes from "./routes/music.route.js"
+import cookieParser from "cookie-parser";
+
 
 const app = express();
+app.use(express.json())
+app.use(cookieParser())
 
-
+app.use("/api/music", musicRoutes)
 
 export default app;
--- music/src/config/config.js
+++ music/src/config/config.js
@@ -4,7 +4,8 @@ import { config as configDotenv } from "dotenv"
 configDotenv();
 
 const _config = {
-  MONGO_URI: process.env.MONGO_URI
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET
 }
 
-export default _config;
+export default Object.freeze(_config);
